e5c2b5519bff235570305a7971f6843b3cd914b4
juanpahdz <juanpahdz01@gmail.com>
2021-06-15 22:23:54 -0500

    Add Cors

--- application.py
+++ application.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, redirect, url_for, jsonify
 from functools import wraps
+from flask_cors import CORS, cross_origin
 import pymongo
 from Routes.userRoutes import users
 from Routes.apartmentRoutes import apartments
 
 application = Flask(__name__)
 application.secret_key = "123askldaskfbbaskdkasgjash0120fd17237ajkhafkas"
+
+CORS(application, resources={r"/*": {"origins": "*"}})
 
 application.register_blueprint(apartments)
 application.register_blueprint(users)
